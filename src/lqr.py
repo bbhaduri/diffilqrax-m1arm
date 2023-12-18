8e4820a1ca44d98cc0ef1767ece98be791859bab
Thomas <thomasmullen96@gmail.com>
2023-12-18 13:49:13 +0000

    pseudo code

--- src/lqr.py
+++ src/lqr.py
@@ -1,3 +1,26 @@
-"""Implementation of LQR rollout function"""
+"""LQR solver via dynamic programming"""
+from typing import Callable, NamedTuple
 import jax
 import jax.lax as lax
+import jax.numpy as np
+
+# symmetrise
+symmetrise = lambda x: (x + x.T) / 2
+
+
+# LQR struct
+class LQR(NamedTuple):
+    pass
+
+
+# forward pass
+
+
+# backward pass
+
+
+# lqr solve
+
+
+if __name__ == "__main__":
+    pass
