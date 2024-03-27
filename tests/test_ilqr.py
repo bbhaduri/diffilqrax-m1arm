fe97c242e6f767dc79f9da7b6ddae566808b89c1
Thomas <thomasmullen96@gmail.com>
2024-03-27 14:49:17 +0000

    add ilqr test skeleton

---
+++ tests/test_ilqr.py
@@ -0,0 +1,35 @@
+"""Test functions in src/ilqr.py"""
+import unittest
+import pytest
+import chex, jax 
+from jax import Array, grad
+import jax.random as jr
+import jax.numpy as jnp
+import numpy as onp
+from os import getcwd
+from pathlib import Path
+
+class TestiLQRStructs(unittest.TestCase):
+    """Test LQR dimensions and data structures"""
+    def setUp(self):
+        pass
+    
+    def test_1(self):
+        pass
+    def test_2(self):
+        pass
+    def test_3(self):
+        pass
+    
+
+class TestiLQRExactSolution(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_1(self):
+        pass
+    def test_2(self):
+        pass
+    def test_3(self):
+        pass
+    
